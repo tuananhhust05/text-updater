4349ccac621c57b810551978d753056baa489d27
Tuấn Anh Nguyễn <tuananhhust05@gmail.com>
2022-07-19 10:34:37 +0700

    update

--- src/js/update.js
+++ src/js/update.js
@@ -18,8 +18,8 @@ const updateOptions = {
     gitRepo: "text-updater", // [Required] Your Repo Name
     gitUsername: "tuananhhust05",  // [Required] Your GitHub Username.
 
-    appName: "test-app", //[Required] The Name of the app archive and the app folder.
-    appExecutableName: "test-app.exe", //[Required] The Executable of the Application to be Run after updating.
+    appName: "example-autoupdate", //[Required] The Name of the app archive and the app folder.
+    appExecutableName: "example-autoupdate.exe", //[Required] The Executable of the Application to be Run after updating.
 
     progressBar: document.getElementById("download"), // {Default is null} [Optional] If Using Electron with a HTML Progressbar, use that element here, otherwise ignore
     label: document.getElementById("download-label"), // {Default is null} [Optional] If Using Electron, this will be the area where we put status updates using InnerHTML
